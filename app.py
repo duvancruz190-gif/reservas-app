ded34cac392709a98f0b62f2aff02725d1cef8fc
duvancruz190-gif <duvancruz190@gmail.com>
2026-05-13 22:42:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -659,8 +659,28 @@ else:
                         ruta,
                         f"reservas/archivo/{a}/{f}"
                     )
+                    
+                    # ===== ACTUALIZAR METADATA =====
+                    ruta_json = f"reservas/enviados/{a}/{f}.json"
 
-                    st.rerun()
+                    if os.path.exists(ruta_json):
+
+                        with open(ruta_json, "r") as jf:
+                             metadata = json.load(jf)
+
+                    metadata["estado"] = "Entregado"
+
+                    metadata["fecha_entrega"] = hora_colombia().strftime(
+                        "%Y-%m-%d %I:%M %p"
+                    )
+
+                    metadata["entregado_por"] = st.session_state.user_name
+
+                    with open(ruta_json, "w") as jf:
+                        json.dump(metadata, jf, indent=4)
+
+                st.success("✅ Documento entregado")
+                st.rerun()
 
                 # ===== ELIMINAR =====
                 if col4.button("🗑️", key=f"del_f_{a}_{f}_{i}"):
